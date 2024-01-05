92df32bb5da8e516bdcb271cfce83897df77409f
KarolinaGroszewska <karolina.groszewska@gmail.com>
2024-01-05 14:11:55 -0600

    intial commit

---
+++ Cheerify/CheerifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheerifyApp.swift
+//  Cheerify
+//
+//  Created by Kari on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct CheerifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
