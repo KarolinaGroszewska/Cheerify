56e1e6133c3c88ef7af336506ddd08517963b07b
Yashraj <yashrajjadhav49@gmail.com>
2024-03-18 22:26:13 +0530

    updated required changes

---
+++ Cheerify/PopUpView.swift
@@ -0,0 +1,18 @@
+//
+//  PopUpView.swift
+//  Cheerify
+//
+//  Created by Yashraj jadhav on 14/03/24.
+//
+
+import SwiftUI
+
+struct PopUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PopUpView()
+}
